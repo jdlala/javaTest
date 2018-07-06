98cda6737ed52aed99a0c2267e27700f58f1c2f3
李傲 <ao.li@jsxfedu.com>
2018-07-06 11:48:23 +0800

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MainClass {
+    private static int flag = 0;
+    public static void main(String[] args) throws Exception {
+
+        final Lock lock = new ReentrantLock();
+
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (flag <= 30) {
+                    lock.lock();
+                    if (flag % 3 == 0) {
+                        System.out.println("A");
+                        flag ++;
+                    }
+                    lock.unlock();
+                }
+            }
+        });
+        Thread b = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (flag <= 30) {
+                    lock.lock();
+                    if (flag % 3 == 1) {
+                        System.out.println("B");
+                        flag ++;
+                    }
+                    lock.unlock();
+                }
+            }
+        });
+        Thread c = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (flag <= 30) {
+                    lock.lock();
+                    if (flag % 3 == 2) {
+                        System.out.println("C");
+                        flag ++;
+                    }
+                    lock.unlock();
+                }
+            }
+        });
+        a.start();
+        b.start();
+        c.start();
+    }
+}
